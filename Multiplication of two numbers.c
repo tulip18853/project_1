d7034641481ceff1d6c1f74a7e5f03d3c5f3fedf
tulip18853 <kupekarshravaani69@gmail.com>
2026-02-23 21:02:38 +0530

    Create Multiplication of two numbers.c

---
+++ Multiplication of two numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    float no1,no2,prod;
+    printf("enter the two numbers");
+    scanf("%f%f",&no1,&no2);
+    prod=no1*no2;
+    printf("the result is %f",prod);
+    getch();
+} 
