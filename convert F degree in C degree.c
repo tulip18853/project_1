cb3128f43f2b3965b0059a9513daa4c9d4063aae
tulip18853 <kupekarshravaani69@gmail.com>
2026-02-23 22:36:11 +0530

    Create convert F degree in C degree.c

---
+++ convert F degree in C degree.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    double f,c;
+    printf("enter the temperature in c");
+    scanf("%lf",&c);
+    c=(f-32)*5/9;
+    printf("the temeprature in celcius is %lf",c);
+    getch();
+    
+}
