0620ee8fa23f775d109219c0549b12fb94f77d7e
tulip18853 <kupekarshravaani69@gmail.com>
2026-02-23 20:59:11 +0530

    Create Subtraction of two numbers.c

---
+++ Subtraction of two numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int a,b,diff;
+    printf("enter the two numbers");
+    scanf("%d%d",&a,&b);
+    diff=a-b;
+    printf("the result is %d",diff);
+    getch();
+}
