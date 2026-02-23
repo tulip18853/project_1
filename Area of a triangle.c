49c1aac18a1392578c3e74895111db9042c55ffb
tulip18853 <kupekarshravaani69@gmail.com>
2026-02-23 21:09:38 +0530

    Create Area of a triangle.c

---
+++ Area of a triangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    float base,height,area;
+    printf("enter the height and base of the triangle ");
+    scanf("%f%f",&height,&base);
+    area=height*base/2;
+    printf("the arae of the triangle is %f",area);
+  getch();
+} 
